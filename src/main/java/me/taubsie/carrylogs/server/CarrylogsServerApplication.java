859ebb434d5633382bbcf4ba04bc99db0b9f7b6c
Taubsie <tobias@joerg-hartmann.de>
2023-04-08 17:09:38 +0200

    Fixed purge command execution.

--- src/main/java/me/taubsie/carrylogs/server/CarrylogsServerApplication.java
+++ src/main/java/me/taubsie/carrylogs/server/CarrylogsServerApplication.java
@@ -1,6 +1,7 @@
 package me.taubsie.carrylogs.server;
 
 import me.taubsie.dungeonhub.common.ClassLoaderService;
+import me.taubsie.dungeonhub.common.OnStart;
 import me.taubsie.dungeonhub.common.ProgramOrigin;
 import me.taubsie.dungeonhub.common.config.ConfigService;
 import me.taubsie.dungeonhub.common.config.ConfigType;
@@ -17,7 +18,7 @@ public class CarrylogsServerApplication extends ProgramOrigin {
     public static void main(String[] args) {
         //TODO fix -> class name start with "BOOT_INF.classes." also
         //ClassLoaderService.getInstance().loadStartupListeners();
-        ClassLoaderService.getInstance().addStartupListener(ConfigService.getInstance());
+        ClassLoaderService.getInstance().addStartupListener(ConfigService.getInstance(), ConfigService.class.getAnnotation(OnStart.class));
         ClassLoaderService.getInstance().executeStartup(new CarrylogsServerApplication());
 
         if(DatabaseService.getInstance().hasInvalidConfigValues()) {
