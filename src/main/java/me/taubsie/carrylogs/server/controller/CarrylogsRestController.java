bfdb05341e3ff69d7b9fc55f7a99d557985dcf01
traible <traible@apollon.de>
2023-05-17 13:16:14 +0200

    Added query parameters to the endpoint for approving-queue and log-queue.

--- src/main/java/me/taubsie/carrylogs/server/controller/CarrylogsRestController.java
+++ src/main/java/me/taubsie/carrylogs/server/controller/CarrylogsRestController.java
@@ -77,15 +77,14 @@ public class CarrylogsRestController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    //TODO change url to use query parameters
-    @GetMapping(value = {"approving-queue", "approving-queue/{id}"})
-    public ResponseEntity<String> getApprovingQueue(@PathVariable(required = false) String id) {
+    @GetMapping(value = {"approving-queue"})
+    public ResponseEntity<String> getApprovingQueue(@RequestParam(required = false) Optional<String> id) {
         try {
-            if(id == null) {
+            if(id.isEmpty()) {
                 return new ResponseEntity<>(CarryLogService.getInstance().getGson().toJson(DatabaseService.getInstance().getApprovingQueue()), HttpStatus.OK);
             }
 
-            Long idLong = Long.parseLong(id);
+            Long idLong = Long.parseLong(id.get());
             return new ResponseEntity<>(CarryLogService.getInstance().getGson().toJson(DatabaseService.getInstance().getFromApprovingQueue(idLong), CarryLogService.getInstance().getCarryInformationSetType()), HttpStatus.OK);
         }
         catch(NumberFormatException numberFormatException) {
@@ -97,9 +96,8 @@ public class CarrylogsRestController {
         }
     }
 
-    //TODO change url to use query parameters
-    @GetMapping(value = {"log-queue", "log-queue/{id}"})
-    public ResponseEntity<String> getLogQueue(@PathVariable(required = false) Optional<String> id) {
+    @GetMapping(value = {"log-queue"})
+    public ResponseEntity<String> getLogQueue(@RequestParam(required = false) Optional<String> id) {
         try {
             if(id.isEmpty()) {
                 return new ResponseEntity<>(CarryLogService.getInstance().getGson().toJson(DatabaseService.getInstance().getLogQueue()), HttpStatus.OK);
