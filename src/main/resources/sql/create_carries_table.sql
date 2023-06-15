afa5b45558e1600284132169f4671d219a03222a
Taubsie <tobias@joerg-hartmann.de>
2023-06-15 18:36:17 +0200

    Added more sql statements for the new score system

--- src/main/resources/sql/create_carries_table.sql
+++ src/main/resources/sql/create_carries_table.sql
@@ -2,7 +2,7 @@ create table carries
 (
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
     serverId        BIGINT NOT NULL,
-    carrier         BIGINT REFERENCES carrier(id),
+    carrier         BIGINT REFERENCES carrier (id),
     player          BIGINT,
     approver        BIGINT,
     amountOfCarries mediumint,
@@ -16,7 +16,7 @@ create table log_queue
 (
     id              BIGINT NOT NULL,
     serverId        BIGINT NOT NULL,
-    carrier         BIGINT REFERENCES carrier(id),
+    carrier         BIGINT REFERENCES carrier (id),
     player          BIGINT,
     amountOfCarries mediumint,
     carryDifficulty varchar(50),
@@ -28,7 +28,7 @@ create table log_approving_queue
 (
     id              BIGINT NOT NULL,
     serverId        BIGINT NOT NULL,
-    carrier         BIGINT REFERENCES carrier(id),
+    carrier         BIGINT REFERENCES carrier (id),
     player          BIGINT,
     amountOfCarries mediumint,
     carryDifficulty varchar(50),
@@ -39,83 +39,112 @@ create table log_approving_queue
 
 create table dungeon_score
 (
-    id       BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
     score    BIGINT,
-    primary key(id,serverId)
+    primary key (id, serverId)
 );
 
 create table slayer_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table kuudra_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table alltime_dungeon_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table alltime_slayer_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table alltime_kuudra_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table event_dungeon_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table event_slayer_score
 (
-    id    BIGINT NOT NULL REFERENCES carrier(id),
+    id       BIGINT NOT NULL REFERENCES carrier (id),
     serverId BIGINT NOT NULL,
-    score BIGINT,
-    primary key(id,serverId)
+    score    BIGINT,
+    primary key (id, serverId)
 );
 
 create table strikes
 (
-    id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    serverId BIGINT NOT NULL,
-    user BIGINT NOT NULL,
-    striker BIGINT,
-    reason varchar(250),
-    time TIMESTAMP NOT NULL
+    id       BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    serverId BIGINT                NOT NULL,
+    user     BIGINT                NOT NULL,
+    striker  BIGINT,
+    reason   varchar(250),
+    time     TIMESTAMP             NOT NULL
 );
+
+create table score
+(
+    id         BIGINT NOT NULL REFERENCES carrier (id) on delete cascade on update cascade,
+    carry_type BIGINT NOT NULL REFERENCES carry_type (id) on delete cascade on update cascade,
+    score      BIGINT,
+    primary key (id, carry_type)
+);
+
+create table alltime_score
+(
+    id         BIGINT NOT NULL REFERENCES carrier (id) on delete cascade on update cascade,
+    carry_type BIGINT NOT NULL REFERENCES carry_type (id) on delete cascade on update cascade,
+    score      BIGINT,
+    primary key (id, carry_type)
+);
+
+create table event_score
+(
+    id         BIGINT NOT NULL REFERENCES carrier (id) on delete cascade on update cascade,
+    carry_type BIGINT NOT NULL REFERENCES carry_type (id) on delete cascade on update cascade,
+    score      BIGINT,
+    primary key (id, carry_type)
+);
+
+insert into score(id, score, carry_type)
+select old_score.id, score, ct.id
+from slayer_score as old_score
+         left join carry_type ct on ct.id = 5;
 
 create table carry_type
 (
-    id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    identifier varchar(250),
-    server BIGINT NOT NULL,
-    logChannel BIGINT
+    id         BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    identifier varchar(50),
+    server     BIGINT                NOT NULL,
+    logChannel BIGINT,
     UNIQUE (identifier, server)
 );
