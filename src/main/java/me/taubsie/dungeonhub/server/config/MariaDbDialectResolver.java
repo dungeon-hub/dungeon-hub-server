600208a2cb34fc9584c05a3e1ca86e4a95adf189
traible <traible@apollon.de>
2023-07-27 10:11:21 +0200

    Updated lombok to the newest version together with some IDE changes, mostly refactors

--- src/main/java/me/taubsie/dungeonhub/server/config/MariaDbDialectResolver.java
+++ src/main/java/me/taubsie/dungeonhub/server/config/MariaDbDialectResolver.java
@@ -3,7 +3,7 @@ package me.taubsie.dungeonhub.server.config;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jdbc.repository.config.DialectResolver;
 import org.springframework.data.relational.core.dialect.Dialect;
-import org.springframework.data.relational.core.dialect.MariaDbDialect;
+import org.springframework.data.relational.core.dialect.MySqlDialect;
 import org.springframework.jdbc.core.JdbcOperations;
 
 import java.util.Optional;
@@ -12,6 +12,7 @@ public class MariaDbDialectResolver implements DialectResolver.JdbcDialectProvid
 {
     @Override
     public @NotNull Optional<Dialect> getDialect(@NotNull JdbcOperations jdbcOperations) {
-        return Optional.of(MariaDbDialect.INSTANCE);
+        //This is the same as mariadb dialect, they have the same instance
+        return Optional.of(MySqlDialect.INSTANCE);
     }
 }
