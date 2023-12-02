0855d47cc03df3ab3ff24c175e58b6c10ebe19dd
Taubsie <tobias@joerg-hartmann.de>
2023-12-02 14:21:38 +0100

    Fixed a few errors that show up in the log.

--- src/main/java/me/taubsie/dungeonhub/server/entities/RefreshTokenId.java
+++ src/main/java/me/taubsie/dungeonhub/server/entities/RefreshTokenId.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -17,4 +18,17 @@ import java.io.Serializable;
 public class RefreshTokenId implements Serializable {
     @Column(name = "user", nullable = false)
     private Long user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RefreshTokenId that = (RefreshTokenId) o;
+        return Objects.equals(user, that.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user);
+    }
 }
