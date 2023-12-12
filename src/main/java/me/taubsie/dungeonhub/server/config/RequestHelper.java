be07ebdb374c2a96f63f7a25f089c0a26f7d3ae7
traible <traible@apollon.de>
2023-12-12 13:28:07 +0100

    Refactored a lot of code and removed useless warnings. Also bumped Spring Boot to 3.2.0

--- src/main/java/me/taubsie/dungeonhub/server/config/RequestHelper.java
+++ src/main/java/me/taubsie/dungeonhub/server/config/RequestHelper.java
@@ -13,6 +13,8 @@ public class RequestHelper {
     private final HttpServletRequest httpServletRequest;
 
     public Object getPathVariable(String name) {
+        //here we have to pray that this will stay the correct type
+        @SuppressWarnings("unchecked")
         final Map<String, Object> pathVariables = (Map<String, Object>) httpServletRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
         return pathVariables.get(name);
     }
