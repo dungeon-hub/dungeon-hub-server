1d6147a62b766d411e847868286d272bb19e89d9
traible <traible@apollon.de>
2024-04-21 10:59:12 +0200

    Removed the call to the config from the test sources since it's not needed there.

--- src/test/java/me/taubsie/dungeonhub/server/DungeonHubServerApplicationDev.java
+++ src/test/java/me/taubsie/dungeonhub/server/DungeonHubServerApplicationDev.java
@@ -1,6 +1,5 @@
 package me.taubsie.dungeonhub.server;
 
-import me.taubsie.dungeonhub.server.config.ConfigService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,8 +8,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan("me.taubsie.dungeonhub.*")
 public class DungeonHubServerApplicationDev {
     public static void main(String[] args) {
-        ConfigService.ensureConfigFile();
-
         SpringApplication.run(DungeonHubServerApplicationDev.class, args);
     }
 }
