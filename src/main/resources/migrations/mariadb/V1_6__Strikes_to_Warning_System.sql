ac39c6ad82b038ab49841b2b5515a9c3b878e8f0
traible <traible@apollon.de>
2024-07-15 01:08:47 +0200

    Fixed the migration for mariadb not working.

--- src/main/resources/migrations/mariadb/V1_6__Strikes_to_Warning_System.sql
+++ src/main/resources/migrations/mariadb/V1_6__Strikes_to_Warning_System.sql
@@ -2,26 +2,33 @@ use `dungeon-hub`;
 
 create table warns
 (
-    id           BIGINT AUTO_INCREMENT               NOT NULL PRIMARY KEY,
-    server_id    BIGINT REFERENCES server (id)       NOT NULL,
-    warned_user       BIGINT REFERENCES discord_user (id) NOT NULL,
-    striker      BIGINT REFERENCES discord_user (id) NOT NULL,
-    warning_type BIGINT                              NOT NULL,
+    id           BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    server_id    BIGINT                NOT NULL REFERENCES server (id),
+    warned_user  BIGINT                NOT NULL REFERENCES discord_user (id),
+    striker      BIGINT                NOT NULL REFERENCES discord_user (id),
+    warning_type BIGINT                NOT NULL,
     reason       varchar(250),
-    active       BOOLEAN                             NOT NULL,
-    time         TIMESTAMP                           NOT NULL
+    active       BOOLEAN               NOT NULL,
+    time         TIMESTAMP             NOT NULL
 );
 
 create table warn_proofs
 (
-    id        BIGINT                              NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    warn_id   BIGINT                              NOT NULL REFERENCES warns (id),
-    proof     varchar(200)                        NOT NULL,
-    submitter BIGINT REFERENCES discord_user (id) NOT NULL
+    id        BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    warn_id   BIGINT                NOT NULL REFERENCES warns (id),
+    proof     varchar(200)          NOT NULL,
+    submitter BIGINT                NOT NULL REFERENCES discord_user (id)
 );
 
 insert into warns(id, server_id, warned_user, striker, warning_type, reason, active, time)
-select strikes.id, strikes.server_id, strikes.`user`, strikes.striker, 0, strikes.reason, true, strikes.time
+select strikes.id,
+       strikes.server_id,
+       strikes.`user`,
+       strikes.striker,
+       0,
+       strikes.reason,
+       true,
+       strikes.time
 from strikes;
 
 drop table if exists strikes;
