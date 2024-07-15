37e2b07e885a4e39a2acf106f14a2101de516524
traible <traible@apollon.de>
2024-07-15 21:43:05 +0200

    Added the possibility to add evidences to warnings.

--- src/main/java/me/taubsie/dungeonhub/server/controller/WarningController.java
+++ src/main/java/me/taubsie/dungeonhub/server/controller/WarningController.java
@@ -2,14 +2,18 @@ package me.taubsie.dungeonhub.server.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import me.taubsie.dungeonhub.common.model.warning.DetailedWarningModel;
 import me.taubsie.dungeonhub.common.model.warning.WarningCreationModel;
+import me.taubsie.dungeonhub.common.model.warning.WarningEvidenceCreationModel;
 import me.taubsie.dungeonhub.common.model.warning.WarningModel;
 import me.taubsie.dungeonhub.server.entities.DiscordServer;
 import me.taubsie.dungeonhub.server.entities.DiscordUser;
 import me.taubsie.dungeonhub.server.entities.Warning;
+import me.taubsie.dungeonhub.server.model.WarningEvidenceInitializeModel;
 import me.taubsie.dungeonhub.server.model.WarningInitializeModel;
 import me.taubsie.dungeonhub.server.service.DiscordServerService;
 import me.taubsie.dungeonhub.server.service.DiscordUserService;
+import me.taubsie.dungeonhub.server.service.WarningEvidenceService;
 import me.taubsie.dungeonhub.server.service.WarningService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -27,36 +31,37 @@ public class WarningController {
     private final WarningService warningService;
     private final DiscordServerService discordServerService;
     private final DiscordUserService discordUserService;
+    private final WarningEvidenceService warningEvidenceService;
 
     @GetMapping("all")
-    public List<WarningModel> getAllWarningsForUser(@PathVariable long server, @RequestParam long user) {
+    public List<DetailedWarningModel> getAllWarningsForUser(@PathVariable long server, @RequestParam long user) {
         return warningService.findAllWarningsForUser(server, user).stream()
-                .map(Warning::toModel)
+                .map(Warning::toDetailedModel)
                 .toList();
     }
 
     @GetMapping("active")
-    public List<WarningModel> getAllActiveWarningsForUser(@PathVariable long server, @RequestParam long user) {
+    public List<DetailedWarningModel> getAllActiveWarningsForUser(@PathVariable long server, @RequestParam long user) {
         return warningService.findAllActiveWarningsForUser(server, user).stream()
-                .map(Warning::toModel)
+                .map(Warning::toDetailedModel)
                 .toList();
     }
 
     @GetMapping("{id}")
-    public WarningModel getWarningById(@PathVariable long server, @PathVariable long id) {
+    public DetailedWarningModel getWarningById(@PathVariable long server, @PathVariable long id) {
         return warningService.loadEntityById(id)
                 .filter(warningModel -> warningModel.getServer().getId() == server)
-                .map(Warning::toModel)
+                .map(Warning::toDetailedModel)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("{id}")
-    public WarningModel deactivateWarning(@PathVariable long server, @PathVariable long id) {
+    public DetailedWarningModel deactivateWarning(@PathVariable long server, @PathVariable long id) {
         Warning warning = warningService.loadEntityById(id)
                 .filter(w -> w.getServer().getId() == server)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        return warningService.deactivateWarning(warning).toModel();
+        return warningService.deactivateWarning(warning).toDetailedModel();
     }
 
     @PostMapping
@@ -68,5 +73,25 @@ public class WarningController {
         WarningInitializeModel initializeModel = new WarningInitializeModel(discordServer, user, striker).fromCreationModel(creationModel);
 
         return warningService.create(initializeModel);
+    }
+
+    @PutMapping("{id}/evidence")
+    public DetailedWarningModel addEvidence(@PathVariable long server, @PathVariable long id, @RequestBody WarningEvidenceCreationModel creationModel) {
+        DiscordServer discordServer = discordServerService.getOrCreate(server);
+        Warning warning = warningService.loadEntityById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if(warning.getServer().getId() != discordServer.getId()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        DiscordUser submitter = discordUserService.loadEntityOrCreate(creationModel.getSubmitter());
+
+        WarningEvidenceInitializeModel initializeModel = new WarningEvidenceInitializeModel(warning, submitter).fromCreationModel(creationModel);
+
+        warningEvidenceService.create(initializeModel);
+
+        return warningService.loadEntityById(warning.getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.EXPECTATION_FAILED))
+                .toDetailedModel();
     }
 }
--- src/main/java/me/taubsie/dungeonhub/server/entities/Warning.java
+++ src/main/java/me/taubsie/dungeonhub/server/entities/Warning.java
@@ -1,23 +1,23 @@
 package me.taubsie.dungeonhub.server.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import me.taubsie.dungeonhub.common.entity.EntityModelRelation;
 import me.taubsie.dungeonhub.common.enums.WarningType;
+import me.taubsie.dungeonhub.common.model.warning.DetailedWarningModel;
 import me.taubsie.dungeonhub.common.model.warning.WarningModel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "warns", schema = "dungeon-hub")
-@AllArgsConstructor
 @NoArgsConstructor
 public class Warning implements EntityModelRelation<WarningModel> {
     @Id
@@ -51,6 +51,20 @@ public class Warning implements EntityModelRelation<WarningModel> {
     @Column(name = "time", nullable = false)
     private Instant time;
 
+    @OneToMany(mappedBy = "warning")
+    private List<WarningEvidence> evidences;
+
+    public Warning(long id, DiscordServer server, DiscordUser user, DiscordUser striker, WarningType warningType, @Nullable String reason, boolean active, Instant time) {
+        this.id = id;
+        this.server = server;
+        this.user = user;
+        this.striker = striker;
+        this.warningType = warningType;
+        this.reason = reason;
+        this.active = active;
+        this.time = time;
+    }
+
     public Warning(DiscordServer server, DiscordUser user, DiscordUser striker, WarningType warningType, @Nullable String reason, boolean active, Instant time) {
         this.server = server;
         this.user = user;
@@ -69,5 +83,9 @@ public class Warning implements EntityModelRelation<WarningModel> {
     @Override
     public @NotNull WarningModel toModel() {
         return new WarningModel(id, server.toModel(), user.toModel(), striker.toModel(), warningType, reason, active, time);
+    }
+
+    public @NotNull DetailedWarningModel toDetailedModel() {
+        return new DetailedWarningModel(id, server.toModel(), user.toModel(), striker.toModel(), warningType, reason, active, time, evidences.stream().map(WarningEvidence::toModel).toList());
     }
 }
