35b55df1ef1edad28fbf68ec3e470009c4a0cc00
traible <traible@apollon.de>
2024-07-22 21:31:05 +0200

    Added migration that I forgot (whoops)

---
+++ src/main/resources/migrations/mariadb/V1_7__Add_Warning_Punishments.sql
@@ -0,0 +1,12 @@
+use `dungeon-hub`;
+
+create table warning_punishment
+(
+    id           BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    server_id    BIGINT                NOT NULL REFERENCES server (id),
+    warning_type BIGINT                NOT NULL,
+    comparison   BIGINT                NOT NULL,
+    count        INT                   NOT NULL,
+    action       BIGINT                NOT NULL,
+    extra_data   VARCHAR(75)
+);
