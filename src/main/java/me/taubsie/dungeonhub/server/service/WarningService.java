cd4ae7a01d2de3fc7b8358d6ac083c9f5be88b81
traible <traible@apollon.de>
2024-07-24 19:44:17 +0200

    Made strikes automatically expire after three months.

--- src/main/java/me/taubsie/dungeonhub/server/service/WarningService.java
+++ src/main/java/me/taubsie/dungeonhub/server/service/WarningService.java
@@ -2,6 +2,7 @@ package me.taubsie.dungeonhub.server.service;
 
 import lombok.AllArgsConstructor;
 import me.taubsie.dungeonhub.common.entity.EntityService;
+import me.taubsie.dungeonhub.common.enums.WarningType;
 import me.taubsie.dungeonhub.common.exceptions.EntityUnknownException;
 import me.taubsie.dungeonhub.common.model.warning.WarningCreationModel;
 import me.taubsie.dungeonhub.common.model.warning.WarningModel;
@@ -9,6 +10,8 @@ import me.taubsie.dungeonhub.common.model.warning.WarningUpdateModel;
 import me.taubsie.dungeonhub.server.entities.Warning;
 import me.taubsie.dungeonhub.server.model.WarningInitializeModel;
 import me.taubsie.dungeonhub.server.repositories.WarningRepository;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +20,7 @@ import java.util.function.Function;
 
 @Service
 @AllArgsConstructor
+@EnableScheduling
 public class WarningService implements EntityService<Warning, WarningModel, WarningCreationModel, WarningInitializeModel, WarningUpdateModel> {
     private WarningRepository warningRepository;
 
@@ -76,5 +80,16 @@ public class WarningService implements EntityService<Warning, WarningModel, Warn
     @Override
     public Function<Warning, WarningModel> toModel() {
         return Warning::toModel;
+    }
+
+    @Scheduled(cron = "0 0 2 * * *")
+    public void test() {
+        List<Warning> warnings = warningRepository.findAllByActiveAndWarningType(true, WarningType.Strike);
+
+        warnings.stream().filter(Warning::isExpired)
+                .forEach(warning -> {
+                    warning.setActive(false);
+                    warningRepository.save(warning);
+                });
     }
 }
