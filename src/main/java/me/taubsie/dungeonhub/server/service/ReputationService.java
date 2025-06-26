f15dff5ebeb5e41baa4e29c02bfeb032628c955b
PassengersTrain <proxydoesmc@gmail.com>
2025-06-26 09:39:05 +0530

    Update ReputationService.java
    Using the right datatype for userId & implemented method to add reputation

--- src/main/java/me/taubsie/dungeonhub/server/service/ReputationService.java
+++ src/main/java/me/taubsie/dungeonhub/server/service/ReputationService.java
@@ -1,6 +1,8 @@
 package me.taubsie.dungeonhub.server.service;
 
+import me.taubsie.dungeonhub.server.entities.DiscordUser;
 import me.taubsie.dungeonhub.server.entities.Reputation;
+import me.taubsie.dungeonhub.server.repositories.DiscordUserRepository;
 import me.taubsie.dungeonhub.server.repositories.ReputationRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +10,30 @@ import org.springframework.stereotype.Service;
 public class ReputationService {
 
     private final ReputationRepository repRepository;
+    private final DiscordUserRepository userRepository;
 
-    public ReputationService(ReputationRepository reputationRepository) {
-        this.repRepository = reputationRepository;
+    public ReputationService(ReputationRepository repRepository, DiscordUserRepository userRepository) {
+        this.repRepository = repRepository;
+        this.userRepository = userRepository;
     }
 
-    public Reputation findByUserId(int userID) {
-        return repRepository.findByUserId(userID);
-    }
-
-    public Reputation save(Reputation reputation) {
+    public Reputation addReputation(Long userId, String repReason) {
+        DiscordUser discordUser = userRepository.loadEntityOrCreate(userId);
+        Reputation reputation = repRepository.findByUser(discordUser);
+        if (reputation == null) {
+            reputation = new Reputation(discordUser, 1, repReason);
+        } else {
+            reputation.updateRecord(1, repReason);
+        }
         return repRepository.save(reputation);
+    }
+
+    public Reputation findByUserId(Long userId) {
+        DiscordUser user = userRepository.loadEntityOrCreate(userId);
+        if (user == null) {
+            return null;
+        } else {
+            return repRepository.findByUser(user);
+        }
     }
 }
