d81a2512bdd6c4f5b17c2305a72da65049229039
PassengersTrain <proxydoesmc@gmail.com>
2025-06-26 09:37:31 +0530

    Update ReputationRepository.java
    Using the right datatype for userId

--- src/main/java/me/taubsie/dungeonhub/server/repositories/ReputationRepository.java
+++ src/main/java/me/taubsie/dungeonhub/server/repositories/ReputationRepository.java
@@ -1,8 +1,9 @@
 package me.taubsie.dungeonhub.server.repositories;
 
+import me.taubsie.dungeonhub.server.entities.DiscordUser;
 import me.taubsie.dungeonhub.server.entities.Reputation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReputationRepository extends JpaRepository<Reputation, Long> {
-    Reputation findByUserId(int userId);
+        Reputation findByUser(DiscordUser user);
 }
